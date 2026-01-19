58b91437bdc8e30ff214054cf6edbcfb5c619cf9
John Xu <jxujohn@gmail.com>
2026-01-18 20:56:22 -0500

    Initial Commit

---
+++ storekit-subscribe-unit/storekit_subscribe_unitApp.swift
@@ -0,0 +1,17 @@
+//
+//  storekit_subscribe_unitApp.swift
+//  storekit-subscribe-unit
+//
+//  Created by John Xu on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct storekit_subscribe_unitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
